1f9a8b2bdf388385860a4b3174a0496b1e12540f
OHTSUKA Ko-hei <kochizufan@gmail.com>
2015-11-22 12:01:42 +0900

    地理院地図
    地理院地図

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,4 +1,11 @@
-var viewer = new Cesium.Viewer('cesiumContainer');
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    imageryProvider: new Cesium.OpenStreetMapImageryProvider({
+        url: 'http://cyberjapandata.gsi.go.jp/xyz/std/', credit: new Cesium.Credit('地理院タイル', '', 'http:// maps.gsi.go.jp/development/ichiran.html')
+    }),
+    baseLayerPicker: false
+});
+
+//new Cesium.Viewer('cesiumContainer');
 
 var point = viewer.entities;
     point.add({
