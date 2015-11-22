e767c03da564a4f4b5bcd6a7f7b409c415af31b9
OHTSUKA Ko-hei <kochizufan@gmail.com>
2015-11-22 11:46:34 +0900

    Cesium点追加
    Cesium点追加

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,1 +1,8 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+
+var point = viewer.entities;
+    point.add({
+    position : Cesium.Cartesian3.fromDegrees(135.502101, 34.693684), point : {
+    pixelSize : 10,
+    color : Cesium.Color.YELLOW }
+});
