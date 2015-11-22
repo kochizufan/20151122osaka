129bb4c30b3ea9d0406e854e4d786e983b2f55c8
OHTSUKA Ko-hei <kochizufan@gmail.com>
2015-11-22 11:54:29 +0900

    kml追加
    kml追加

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -9,3 +9,6 @@ var point = viewer.entities;
 
 //視点の変更
 viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(135.502101, 34.693684, 15000.0)});
+
+//KMLの追加 
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://seabook0303.github.io/2020151122osaka/cesium-starter-app-master/foss4gosakakml/foss4gosaka.kml'));
