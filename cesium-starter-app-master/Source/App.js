79a58426602eba80f0721c8c0b93d13b6fde8f61
OHTSUKA Ko-hei <kochizufan@gmail.com>
2015-11-22 11:50:50 +0900

    視点追加
    視点追加

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -6,3 +6,6 @@ var point = viewer.entities;
     pixelSize : 10,
     color : Cesium.Color.YELLOW }
 });
+
+//視点の変更
+viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(135.502101, 34.693684, 15000.0)});
