966a0409d8c2f564edb28f8e61cf145d35daa673
OHTSUKA Ko-hei <kochizufan@gmail.com>
2015-11-22 12:04:39 +0900

    タイル戻す
    タイル戻す

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,11 +1,9 @@
-var viewer = new Cesium.Viewer('cesiumContainer', {
+var viewer = /*new Cesium.Viewer('cesiumContainer', {
     imageryProvider: new Cesium.OpenStreetMapImageryProvider({
         url: 'http://cyberjapandata.gsi.go.jp/xyz/std/', credit: new Cesium.Credit('地理院タイル', '', 'http:// maps.gsi.go.jp/development/ichiran.html')
     }),
     baseLayerPicker: false
-});
-
-//new Cesium.Viewer('cesiumContainer');
+});*/ new Cesium.Viewer('cesiumContainer');
 
 var point = viewer.entities;
     point.add({
